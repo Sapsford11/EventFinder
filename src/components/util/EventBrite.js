36196ac03b10c13a422a61c9dcacab53198a8255
Sapsford11 <craig.sapsford11@gmail.com>
2018-06-08 16:46:55 +0200

    Edited API call, changed 'q' for 'term'

--- src/components/util/EventBrite.js
+++ src/components/util/EventBrite.js
@@ -3,7 +3,7 @@ const myToken = '2OHPMQAPHJJPBATY6ECJ';
 
 const EventBrite = {
   search(term, address, date, sortBy) {
-    return fetch(`https://cors-anywhere.herokuapp.com/http://www.eventbriteapi.com/v3/events/search?token=2OHPMQAPHJJPBATY6ECJ&q=${term}&location.address=${address}&start_date.keyword=${date}&sort_by=${sortBy}`, {
+    return fetch(`https://cors-anywhere.herokuapp.com/http://www.eventbriteapi.com/v3/events/search?token=2OHPMQAPHJJPBATY6ECJ&term=${term}&location.address=${address}&start_date.keyword=${date}&sort_by=${sortBy}`, {
       headers: {
         Authorization: `Bearer ${myToken}`
       }
@@ -13,7 +13,7 @@ const EventBrite = {
       if (jsonResponse.events) {
         return jsonResponse.events.map(event => ({
           id: event.id,
-          imageSrc: event.logo_id,
+          imageSrc: event.logo,
           date: event.start,
           name: event.name.text,
           address: event.location.address
