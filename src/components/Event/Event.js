a3670e36edbf6b1b912824584f6882818904d28e
Sapsford11 <craig.sapsford11@gmail.com>
2018-07-23 19:02:05 +0200

    Updated Search bar placeholders

--- src/components/Event/Event.js
+++ src/components/Event/Event.js
@@ -4,18 +4,12 @@ import './Event.css';
 
 class Event extends React.Component {
 
-  eventImage() {
-    if (this.props.event.logo.url === null) {
-            return <img src="no-image-available.png" alt=""/>;
-          }
-    };
-
   render() {
     return (
       <div className="Event">
-          <div className="image-container">
-            <a href={this.props.event.url}target="_blank" rel="noopener noreferrer"><img src={this.props.event.logo} alt='Event logo'/></a>
-          </div>
+      <div className="image-container">
+        <a href={this.props.event.url}target="_blank" rel="noopener noreferrer"><img src={this.props.event.logo} alt="Event logo" /></a>
+      </div>
           <div className="event-card">
             <p className="date-time">{this.props.event.date}</p>
             <div className="event-title">
--- src/components/util/EventBrite.js
+++ src/components/util/EventBrite.js
@@ -2,6 +2,7 @@
 const myToken = '2OHPMQAPHJJPBATY6ECJ';
 
 const EventBrite = {
+
   search(term, location, date, sortBy) {
     return fetch(`https://cors-anywhere.herokuapp.com/http://www.eventbriteapi.com/v3/events/search?token=2OHPMQAPHJJPBATY6ECJ&q=${term}&location.address=${location}&start_date.keyword=${date}&sort_by=${sortBy}`, {
       headers: {
@@ -13,7 +14,7 @@ const EventBrite = {
         }).then(jsonResponse => {
       if (jsonResponse.events) {
         return jsonResponse.events.map(event => ({
-          id: event.venue_id,
+          id: event.id,
           logo: event.logo.url,
           date: event.start.local,
           name: event.name.text,
@@ -21,7 +22,9 @@ const EventBrite = {
         }));
       }
     });
+
   }
+
 }
 
 export default EventBrite;
